7552499734277e5a38cc9d0a3fd5c7800fde0ab8
Malachow, Shane <shane.malachow@sciencelogic.com>
2020-12-08 21:46:48 -0500

    feat: running the program will output next semver for repo

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"regexp"
 	"strings"
 
 	"github.com/go-git/go-git/v5"
@@ -10,7 +12,14 @@ import (
 	semver "github.com/shanemalachow/AutoSemVer/SemVer"
 )
 
+const commitRegex = `^((?P<type>\w*)(\((?P<area>.+)\))?: ?)?(?P<msg>.*)`
+
 func main() {
+	r := regexp.MustCompile(commitRegex)
+	major := append([]string{"major", "maj"}, strings.Split(os.Getenv("MAJOR"), ", ")...)
+	minor := append([]string{"minor", "min"}, strings.Split(os.Getenv("MINOR"), ", ")...)
+	patch := append([]string{"patch", "pat"}, strings.Split(os.Getenv("PATCH"), ", ")...)
+
 	repo, err := git.PlainOpen(".")
 	if err != nil {
 		panic(err)
@@ -22,10 +31,12 @@ func main() {
 	}
 
 	versions := []semver.SemVer{}
+	commitMsgs := []string{}
 
-	fmt.Println("Trying to find tags in order")
+	// fmt.Println("Trying to find tags in order")
 	if commits.ForEach(func(c *object.Commit) error {
 		// fmt.Println("Commit: " + c.Hash.String())
+		commitMsgs = append(commitMsgs, c.Message)
 		tagsIter, err := repo.Tags()
 		err = tagsIter.ForEach(func(t *plumbing.Reference) error {
 			// fmt.Println(t)
@@ -51,5 +62,45 @@ func main() {
 	}) != nil {
 		panic(err)
 	}
-	versions[0].Print()
+
+	highestChange := "noop"
+	for _, s := range commitMsgs {
+		groups := r.FindStringSubmatch(s)
+		if groups[2] != "" {
+			for _, maj := range major {
+				if groups[2] == maj {
+					highestChange = "major"
+				}
+			}
+			if highestChange != "major" {
+				for _, min := range minor {
+					if groups[2] == min {
+						highestChange = "minor"
+					}
+				}
+			} else {
+				if highestChange != "minor" {
+					for _, pat := range patch {
+						if groups[2] == pat {
+							highestChange = "patch"
+						}
+					}
+				}
+			}
+		}
+	}
+	nextVer := versions[0]
+	// fmt.Println(nextVer.Version())
+	switch highestChange {
+	case "major":
+		nextVer.Major++
+		nextVer.Minor = 0
+		nextVer.Patch = 0
+	case "minor":
+		nextVer.Minor++
+		nextVer.Patch = 0
+	case "patch":
+		nextVer.Patch++
+	}
+	fmt.Println(nextVer.Version())
 }
